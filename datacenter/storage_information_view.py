8b9857ce9912b0189f4c7ae5f41c1b6bbd071bf6
kmmorozov <kmmorozov@gmail.com>
2022-09-07 16:01:43 +0300

    Избавился от лишних переменных

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -2,20 +2,17 @@ from datacenter.models import Visit
 from django.shortcuts import render
 from datacenter.models import get_duration
 from datacenter.models import format_duration
-from django.utils.timezone import localtime
+
 
 
 def storage_information_view(request):
     non_closed_visits = []
     not_end_visit = Visit.objects.filter(leaved_at__isnull=True)
     for visit in not_end_visit:
-        duration = get_duration(visit)
-        formatted_duration = format_duration(duration)
-        moscow_entered_time = f'{localtime(visit.entered_at).date()} {localtime(visit.entered_at).time()} '
         visit_info = {
-            'who_entered': '{}'.format(visit.passcard.owner_name),
-            'entered_at': '{}'.format(moscow_entered_time),
-            'duration': '{}'.format(formatted_duration),
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': format_duration(get_duration(visit)),
         }
         non_closed_visits.append(visit_info)
 
