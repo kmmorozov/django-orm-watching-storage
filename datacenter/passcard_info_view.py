9796f97a6b73a90cb135804bc0b5a0e551cb75d0
kmmorozov <kmmorozov@gmail.com>
2022-09-10 20:14:48 +0300

    Убрал лишнее форматирование.

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from datacenter.models import get_duration
 from datacenter.models import format_duration
 from datacenter.models import is_visit_long
-from django.utils.timezone import localtime
 from django.shortcuts import get_list_or_404
 from django.shortcuts import get_object_or_404
 
@@ -14,19 +13,12 @@ def passcard_info_view(request, passcode):
     visits_from_passcard = get_list_or_404(Visit, passcard=onwer_name)
     this_passcard_visits = []
     for visit in visits_from_passcard:
-        entered_date = localtime(visit.entered_at).date()
-        entered_time = localtime(visit.entered_at).time()
-        entered_at = 'дата: {}   время: {}'.format(entered_date,entered_time)
-        long_visit = is_visit_long(visit)
-        duration = get_duration(visit)
-        formated_duration = format_duration(duration)
-        this_passcard_visits.append(
-            {
-                'entered_at': '{}'.format(entered_at),
-                'duration': '{}'.format(formated_duration),
-                'is_strange': '{}'.format(long_visit)
-            }
-        )
+        visit_info = {
+            'entered_at': visit.entered_at,
+            'duration': format_duration(get_duration(visit)),
+            'is_strange': is_visit_long(visit)
+        }
+        this_passcard_visits.append(visit_info)
     context = {
         'passcard': onwer_name,
         'this_passcard_visits': this_passcard_visits
